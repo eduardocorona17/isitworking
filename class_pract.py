81b3e671ba792884a9b3e4c17faebd301e73efe8
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-18 22:39:50 -0800

    Car Class Structure

--- class_pract.py
+++ class_pract.py
@@ -15,3 +15,22 @@ class Dog:
 my_dog = Dog("Sammi", 3)
 # print(my_dog.bark())
 my_dog.bark()
+my_dog.sit()
+your_dog = Dog("Luna", 4)
+your_dog.bark()
+
+class Car:
+    """Emulate a vehicle"""
+    def __init__(self, make, model, color):
+        """Initialize make, model, color"""
+        self.make = make
+        self.model = model
+        self.color = color
+        self.accelarate = ""
+    
+    def vroom(self):
+        print(f"My {self.model} go vroom lol!")
+
+F_150 = Car("Ford", "F-150", "Black")
+F_150.vroom()
+
