731d71f70862a9ccd230e487aa268ad69e301ab7
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-21 14:41:05 -0800

    Back to basics

---
+++ var.py
@@ -0,0 +1,9 @@
+message = "Hello Python World!"
+print(message)
+message = "What up Python Crash Course!"
+print(message)
+
+simple_message = "Simple enough?"
+print(simple_message)
+simple_message = "This is also simple!"
+print(simple_message)
