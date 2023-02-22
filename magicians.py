e1c73dac5ef4810b65d8bfda0cb9229fc8521ab8
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-22 13:54:32 -0800

    Refresh the basics

---
+++ magicians.py
@@ -0,0 +1,5 @@
+magicians = ['eduardo', 'ivan', 'taylor', 'david']
+for magician in magicians:
+    print(f"{magician.title()} that was a neat trick!")
+    print(f"Looking forward to your next trick, {magician.title()}.\n")
+print(f"Thanks for the show! {magicians[0].title()} was the best!")
--- var.py
+++ var.py
@@ -7,3 +7,40 @@ simple_message = "Simple enough?"
 print(simple_message)
 simple_message = "This is also simple!"
 print(simple_message)
+name = "ada lovelace"
+print(name.title())
+first_name = "eduardo"
+last_name = "corona"
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
+
+x, y, z = 1, 2, 3
+print(y)
+number_list = ['1', '2', '3', '4', '5', '6']
+print(number_list[-3])
+print(number_list[-2])
+print(number_list[-1])
+other_msg = f"My first grade was a {number_list[-2]}/6"
+print(other_msg)
+number_list[0] = '3'
+print(number_list)
+bikes = ['honda', 'yamaha', 'suzuki']
+bikes.append('ducati')
+print(bikes)
+bikes.insert(0, 'brabas')
+print(bikes)
+print(bikes[0].title())
+print(bikes)
+del bikes[3]
+print(bikes)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+sorted(cars)
+print(cars)
+cars.reverse()
+print(cars)
+print(len(cars))
