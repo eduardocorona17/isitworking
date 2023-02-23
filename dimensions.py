93e9e2b7187fc5b499db4edce64beef79a76af9a
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-23 13:23:19 -0800

    Usernames updated

--- dimensions.py
+++ dimensions.py
@@ -20,3 +20,32 @@ buffet = ("Pizza", "Pasta", "Sandwhiches", "Apple Juice", "Milk")
 print("We lost our liquor license: ")
 for food in buffet:
     print(f"{food}")
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car)
+new_users = []
+current_users = ['Eduardocorona', 'aliceabc', 'LILY', 'Clarence']
+user_name = input("Please enter a new user name (enter 'q' to finish): ")
+# while user_name != 'q':
+# if user_name in current_users:
+#     print(f"Error! {user_name} is already used")
+#     print(f"Please enter a {user_name}")
+# else:
+#     new_users.append(user_name)
+# print(new_users)
+# print()
+# for user in current_users:
+#     user.lower()
+#     if user_name in current_users:
+#         print(f"Sorry {user_name} is already taken try again: ")
+#         print(f"Enter {user_name}")
+#     else:
+#         new_users.append(user_name)
+# print(current_users)
+        
+
+
