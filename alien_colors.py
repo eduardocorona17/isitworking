b9d3a7d21f75d181259251571069d7795899d7a2
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-24 14:40:35 -0800

    Life stages cycle complete

---
+++ alien_colors.py
@@ -0,0 +1,20 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("5 Points!")
+if alien_color == 'yellow':
+    print("10 points!")
+if alien_color == 'red':
+    print("holy smokes 15 points!")
+
+alien_color2 = 'yellow'
+if alien_color2 == 'yellow':
+    print("Bro nice job 10 points.")
+if alien_color2 != 'yellow':
+    print("Umm we gotta figure this one out.")
+
+alien_color3 = 'red'
+if alien_color3 == 'red':
+    print("Nice freakin job bro!")
+elif alien_color3 != 'red':
+    print("Oof, maybe next time.")
+    
---
+++ life_stages.py
@@ -0,0 +1,22 @@
+age = 32
+
+if age > 2 and age < 4:
+    print("Child.")
+elif age >= 4 and age < 13:
+    print("Kid.")
+elif age >= 1 and age < 20:
+    print("Teen.")
+elif age >= 20 and age < 65:
+    print("Adult")
+elif age >= 65:
+    print("Its over")
+# if 2 < age < 4:
+#     print("This is a baby still.")
+# elif 4 >= age < 13:
+#     print("This is a kid.")
+# elif 13 >= age < 20:
+#     print("This person is a teenager.")
+# elif 20 >= age < 65:
+#     print("Here we have an adult.")
+# elif age > 65:
+#     print("Its over bro.")
