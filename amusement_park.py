c84a277127f2a8c6b1d6489b1a0881b1c3a802af
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-24 14:11:16 -0800

    Toppings menu complete

--- amusement_park.py
+++ amusement_park.py
@@ -6,10 +6,13 @@
 # else:
 #     print("Admission is $40.")
 
-age = 12
+age = 66
 if age < 4:
     price = 0
 elif age < 18:
     price = 25
-else: price = 40
+elif age < 65:
+    price = 40
+elif age >= 65:
+    price = 20
 print(f"Your admission cost is ${price}")
---
+++ toppings.py
@@ -0,0 +1,9 @@
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushies.")
+if 'pepperoni' in requested_toppings:
+    print("Adding p-p-roni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra queso.")
+print("Donzo with your pizza.")
