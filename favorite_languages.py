45f4e94209586f181a935caca61a69f280b29405
Eduardo Corona <ecoronalopez@gmail.com>
2023-02-25 19:22:13 -0800

    Programming language Dict done

--- favorite_languages.py
+++ favorite_languages.py
@@ -8,8 +8,25 @@ favorite_languages = {
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}")
 
-alien_o = {'color': 'green', 'points':20}
-speed_value = alien_o.get('speed', 'No speed value assigned')
-print(speed_value)
-for name, language in favorite_languages.items():
-    print(f"{name.title()}'s favorite language is {language.title()}")
+for name in favorite_languages.keys():
+    print(f"{name.title()}")
+
+for lang in set(favorite_languages.values()): # set returns python 1s instead of twice.
+    print(f"{lang.title()}")
+
+friends = ['sarah', 'phil']
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"Hi {name.title()}, I see you like {language.title()}!")
+if 'erin' not in favorite_languages.keys():
+    print("Erin, take our poll dude! What language do you like or write in?")
+
+# alien_o = {'color': 'green', 'points':20}
+# speed_value = alien_o.get('speed', 'No speed value assigned')
+# print(speed_value)
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}'s favorite language is {language.title()}")
