d340923f4a45dc05586c6ab616c48439a001c823
Eduardo Corona <ecoronalopez@gmail.com>
2023-03-11 14:07:52 -0800

    Current number while loop

---
+++ cities.py
@@ -0,0 +1,9 @@
+prompt = "\nEnter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finsihed.) "
+
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
--- parrot.py
+++ parrot.py
@@ -1,6 +1,13 @@
 prompt = "\nTell me something to repeat to you: "
 prompt += "\nEnter 'quit' to end the program. "
+active = True
+
 message = ""
-while message != 'quit':
+while active:
+# while message != 'quit':
     message = input(prompt)
-    print(message)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
