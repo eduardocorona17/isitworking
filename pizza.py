483696c32f7d1416d5e0c19076f58291f32614a7
Eduardo Corona <ecoronalopez@gmail.com>
2023-03-16 13:44:31 -0700

    Pizza topping function complete

--- pizza.py
+++ pizza.py
@@ -1,9 +1,16 @@
-pizza = {
-    'crust': 'thick',
-    'toppings': ['mushrooms', 'extra cheese'],
-}
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extra cheese'],
+# }
 
-print(f"You ordered a {pizza['crust']}-crust pizza with the following toppings: ")
-for topping in pizza['toppings']:
-    print(f"\t+ {topping}")
+# print(f"You ordered a {pizza['crust']}-crust pizza with the following toppings: ")
+# for topping in pizza['toppings']:
+#     print(f"\t+ {topping}")
 
+def make_pizza(*toppings):
+    """Summarize pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings: ")
+    for topping in toppings:
+        print(f"- {topping}")
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
--- printing_models.py
+++ printing_models.py
@@ -1,11 +1,27 @@
+
+
+# print("Printing current models: ")
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design} ")
+#     completed_models.append(current_design)
+# print("\nThese designs have been printed: ")
+# for model in completed_models:
+#     print(f"{model.title()}")
 unprinted_designs = ['phone chip', 'machine pendant', 'dodecahedron']
 completed_models = []
 
-print("Printing current models: ")
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design} ")
-    completed_models.append(current_design)
-print("\nThese designs have been printed: ")
-for model in completed_models:
-    print(f"{model.title()}")
+# Modified code below:
+def print_models(unprinted_models, completed_models):
+    while unprinted_designs:
+        current_designs = unprinted_designs.pop()
+        print(f"Printing model: {current_designs}")
+        completed_models.append(current_designs)
+
+def show_completed_models(completed_models):
+    print("\nThe following models have been printed: ")
+    for completed_model in completed_models:
+        print(f"{completed_model}")
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
