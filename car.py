304737519f73c096ee663187abed1c2a9f37e7a7
Eduardo Corona <ecoronalopez@gmail.com>
2023-04-12 11:55:24 -0700

    Fix AMG def

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
 class Car:
     """A simpple attempt to represent a car."""
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, special, year):
         """Initialize attributes to describe a car."""
         self.make = make
         self.model = model
+        self.special = special
         self.year = year
         self.odometer_reading = 0
 
@@ -12,6 +13,14 @@ class Car:
         long_name = f"{self.make} {self.model} {self.year}"
         return long_name.title()
     
+    def is_amg(self):
+        """Tells if it is an AMG or not"""
+        if {self.special} == "AMG":
+            print(f"This {self.make} is an AMG")
+        else:
+            print(f"This {self.make} is not an AMG")    
+
+
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print(f"This car has {self.odometer_reading} miles on it.")
@@ -25,8 +34,10 @@ class Car:
             print("You can't roll back the odometer!")
             
 
-my_new_car = Car('audi', 'r8', 2023)
+my_new_car = Car('audi', 'r8', 'NA',2023)
+amg = Car('Benz', 'CLA', 'AMG', 2023)
 print(my_new_car.get_descriptive_name())
 # my_new_car.odometer_reading = 23
 my_new_car.update_odometer(25)
 my_new_car.read_odometer()
+amg.is_amg()
