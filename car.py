f5d33f133a3e416fddfaa80258134a72e3e30aa5
Eduardo Corona <ecoronalopez@gmail.com>
2023-04-13 12:06:49 -0700

    Fixed AMG

--- car.py
+++ car.py
@@ -1,24 +1,26 @@
 class Car:
     """A simpple attempt to represent a car."""
-    def __init__(self, make, model, special, year):
+    def __init__(self, make, model, year):
         """Initialize attributes to describe a car."""
         self.make = make
         self.model = model
-        self.special = special
         self.year = year
         self.odometer_reading = 0
+        self.special = ''
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name = f"{self.make} {self.model} {self.year}"
         return long_name.title()
-    
-    def is_amg(self):
+    # FIX THIS DEF BELOW
+    def is_amg(self, special):
         """Tells if it is an AMG or not"""
-        if {self.special} == "AMG":
+        if special == 'AMG':
+            special == self.special
             print(f"This {self.make} is an AMG")
         else:
             print(f"This {self.make} is not an AMG")    
+
 
 
     def read_odometer(self):
@@ -34,10 +36,10 @@ class Car:
             print("You can't roll back the odometer!")
             
 
-my_new_car = Car('audi', 'r8', 'NA',2023)
-amg = Car('Benz', 'CLA', 'AMG', 2023)
+my_new_car = Car('audi', 'r8', 2023)
+amg = Car('Benz', 'CLA',2023)
 print(my_new_car.get_descriptive_name())
 # my_new_car.odometer_reading = 23
 my_new_car.update_odometer(25)
 my_new_car.read_odometer()
-amg.is_amg()
+amg.is_amg('CLS')
